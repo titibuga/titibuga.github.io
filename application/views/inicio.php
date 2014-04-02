c9a72ea85598d44fd362b9c8f99d0ff21a684ba3
Victor Portella <visportella@gmail.com>
2014-04-02 19:55:43 -0300

    Atualizado a home

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -4,13 +4,10 @@
 
  <div class="inner-cover">
 
-			<img style="height:auto; width:auto; max-width: 500px;" class = "image-responsive"  src="assets/images/CN.png">   
+			<img style="height:auto; width:auto; max-height: 500px;" class = "image-responsive"  src="assets/images/LogoV.png">   
             <h1 class="cover-heading">Encontro do BCC - 40 anos</h1>
             <p class="lead">Esse ano o curso de Bacharelado em Ciência da Computação na USP completa 40 anos. Para comemorar, 
 							acontecerá uma versão especial do tradicional Encontro do BCC.</p>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">Learn more</a>
-            </p>
     </div><!-- Inner Cover -->
 
 </div><!-- Cover container -->	
@@ -27,7 +24,7 @@
 				</p>
             </div>
             <div class="col-md-5">
-				<img class = "image-responsive" style="height:auto; width:auto; max-height: 300px;"  src="assets/images/pombo.png"> 
+				<img class = "image-responsive" style="height:auto; width:auto; max-height: 300px;"  src="assets/images/OldPC.png"> 
               <!-- <img class="featurette-image img-responsive" data-src="assets/images/pombo.png" alt="Generic placeholder image"> -->
             </div>
           </div>
